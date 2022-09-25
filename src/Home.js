1500ad9163d3b18ecb1b3850b189e19487434250
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-09-25 04:12:09 -0700

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,20 @@
 const Home = () => {
+
+
+    const handleClick = ()=>{
+        alert("hello")
+    };
+    const handleClickAgain = (name)=>{
+        alert("hello "  + name)
+    };
     return (  
 
         <div className="home">
             <h2>HomePage</h2>
+            {/* making the function so it invokes the click */}
+            <button onClick={handleClick}>Click Me!</button>
+            {/* this can invoke names inside the function */}
+            <button onClick={()=>{handleClickAgain("John")}}>Click Me Again!</button>
         </div>
     );
 }
