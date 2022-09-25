32bfd4913686276842a177ba9394c12072ebfd4b
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-09-25 05:06:33 -0700

    using State useState

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,29 @@
+// we start by using state right here
+import {useState} from "react";
+
 const Home = () => {
+
+    // this is the use state feature
+    const [name, setName] = useState('mario')
+    const [age, setAge] = useState(12)
 
 
     const handleClick = ()=>{
-        alert("hello")
+     setName("luigi")
+     setAge(30)
     };
-    const handleClickAgain = (name)=>{
-        alert("hello "  + name)
-    };
+
+
     return (  
 
         <div className="home">
             <h2>HomePage</h2>
             {/* making the function so it invokes the click */}
+            <p>{name} is {age} years old</p>
+            
             <button onClick={handleClick}>Click Me!</button>
             {/* this can invoke names inside the function */}
-            <button onClick={()=>{handleClickAgain("John")}}>Click Me Again!</button>
+
         </div>
     );
 }
