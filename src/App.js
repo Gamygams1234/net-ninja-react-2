7933db8b644cb9192f7328372d161ab0101cb0f7
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-09-25 03:59:26 -0700

    Styling the page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 
-import './App.css';
-import Navbar from './Navbar';
-import Home from './Home';
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-
   // adding our values at the top
-  const title = "Welcome to the new blog";
-  const likes = 50;
+
   return (
     <div className="App">
       <Navbar></Navbar>
-    <Home></Home>
+      <div className="content">
+      <Home></Home>
+      </div>
+   
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
             <h1>The Dojo Blog</h1>
             <dov className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a href="/create" style={{color: "white", backgroundColor: "#f1356d", borderRadius:"8px"}}>New Blog</a>
             </dov>
         </nav>
      );
