68a229010c08690d06677331c394c356e0ca03f4
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-09-25 03:28:52 -0700

    Dynamic values and templates

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
   return (
     <div className="App">
       {/* making our component with our content */}
           <div className="content">
-                <h1>App Component</h1>
+                <h1>{title}</h1>
+                <p className="likes">Liked {likes} times</p>
 
           </div>
     </div>
