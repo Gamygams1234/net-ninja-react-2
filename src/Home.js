edfca8183851fea0a5015855621bdccbd0e6fd6f
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-09-26 18:08:43 -0700

    The dependencies of useEffect

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
     { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
     { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3 },
   ]);
-
+const [name, setName] = useState("mario");
 
 //   filter the blogs and return it. 
 // we are running the function on the data itself
@@ -20,17 +20,30 @@ const Home = () => {
       setBlogs(newBlogs)
 
   }
+  const handleNameChange = () =>{
+    
+    const newName = "John Cena"
+    setName(newName)
+  }
 
 //   use Effect runs at the begining and when the state is changed
+
+// now use effect only runs when the name is changed  because of the dependencies
   useEffect(()=>{
       console.log("use effect ran")
       console.log(blogs)
-  })
+  },[name])
   return (
     <div className="home">
+
+        <h1>
+            Welcome {name}
+        </h1>
       {/* maps will retrun the element for each item in array */}
       <BlogList blogs={blogs} title="All Blogs" handleDelete = {handleDelete}/>
       {/* run the filter in the curly brackets */}
+
+      <button onClick={handleNameChange}>Change name</button>
 
     </div>
   );
