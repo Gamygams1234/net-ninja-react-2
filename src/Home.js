d6b16597627fb245f4bb82284b85222a6ee06552
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-09-26 23:41:48 -0700

    Setting a loading before a fetch

--- src/Home.js
+++ src/Home.js
@@ -3,47 +3,44 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
- 
-
   const [blogs, setBlogs] = useState(null);
-const [name, setName] = useState("mario");
+  const [name, setName] = useState("mario");
+  const [isPending, setIsPending] = useState(true);
 
-// this is to use the json server when we fetch data
-// npx json-server --watch data/db.json --port 8080
+  // this is to use the json server when we fetch data
+  // npx json-server --watch data/db.json --port 8080
 
+  const handleNameChange = () => {
+    const newName = "John Cena";
+    setName(newName);
+  };
 
-  const handleNameChange = () =>{
-    
-    const newName = "John Cena"
-    setName(newName)
-  }
+  //   use Effect runs at the begining and when the state is changed
+  // using use effect for the blogs
 
-//   use Effect runs at the begining and when the state is changed
-// using use effect for the blogs
-
-  useEffect(()=>{
-     fetch('http://localhost:8080/blogs')
-     .then(res=>{
-         return res.json()
-     }).then(data=>{
-         setBlogs(data)
-         console.log(data)
-     })
-  },[])
-
+  useEffect(() => {
+    fetch("http://localhost:8080/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+        console.log(data);
+        setIsPending(false)
+      });
+  }, []);
 
   return (
     <div className="home">
+      <h1>Welcome {name}</h1>
 
-        <h1>
-            Welcome {name}
-        </h1>
-      {/* maps will retrun the element for each item in array */}
-     { blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete />}
+
+    {/* sets a loading message for a little bit */}
+      {isPending && <div>Loading... </div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete />}
       {/* run the filter in the curly brackets */}
 
       <button onClick={handleNameChange}>Change name</button>
-
     </div>
   );
 };
