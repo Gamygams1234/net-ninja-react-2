189b12c51593e49be16aa1dca5ba214acaaaf37a
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-09-28 07:54:39 -0700

    Route parameters

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 // routes is the version 6
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Create from "./Create";
+import BlogDetails from "./BlogDetails";
 
 function App() {
   // adding our values at the top
@@ -15,7 +16,8 @@ function App() {
           {/* routes is the version 6 */}
           <Routes>
             <Route exact path="/" element={<Home />}></Route>
-            <Route exact path="/create" element={<Create />}></Route>
+            <Route path="/create" element={<Create />}></Route>
+            <Route path="/blogs/:id" element={<BlogDetails />}></Route>
           </Routes>
         </div>
       </div>
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BlogList = (props) => {
   const blogs = props.blogs
   const title = props.title
@@ -13,9 +15,10 @@ const BlogList = (props) => {
 {blogs.map((blog) => (
         //   ea
         <div className="blog-preview" key={blog.id}>
+          <Link to = {"/blogs/" + blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
-          <button >Delete Blog</button>
+        </Link>
         </div>
       ))}
 </div>
