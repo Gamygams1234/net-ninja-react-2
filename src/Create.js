34f7bd4a4f02d2f7defcbda72047cbf55a1026c9
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-09-28 16:21:52 -0700

    Making a post request

--- src/Create.js
+++ src/Create.js
@@ -1,31 +1,40 @@
 import { useState } from "react";
 
 const Create = () => {
-    const [title, setTitle] = useState("")
-    const [body, setBody] = useState("")
-    const [name, setName] = useState("Yoshi")
-    const handleSubmit = (e)=>{
-        e.preventDefault()
-        console.log(`Title: ${title}, body: ${body}, Author: ${name}`)
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [name, setName] = useState("Yoshi");
+  const [isPending, setIsPending] = useState(false);
 
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsPending(true)
+    const blog = { title, body, author: name };
+    fetch(`http://localhost:8080/blogs/`, { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(blog) }).then(() => {
+      console.log(`new blog added`);
+      setIsPending(false)
+    });
+  };
   return (
     <div className="create">
       <h2>Add a new Blog</h2>
 
       <form onSubmit={handleSubmit}>
         <label>Blog Title</label>
-        <input type="text" required value = {title} onChange = {(e)=>setTitle(e.target.value)}/>
-   
+        <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
+
         <label>Blog Body</label>
-        <textarea value = {body} onChange = {(e)=>setBody(e.target.value)}  required></textarea>
+        <textarea value={body} onChange={(e) => setBody(e.target.value)} required></textarea>
         <label>Author</label>
-        <select name="" id="" value = {name} onChange = {(e)=>setName(e.target.value)}>
+        <select name="" id="" value={name} onChange={(e) => setName(e.target.value)}>
           <option value="Yoshi">Yoshi</option>
           <option value="Mario">Mario</option>
         </select>
 
-        <button>Add Blog</button>
+{!isPending?
+        <button>Add Blog</button>:
+        <button disabled>Loading..</button>
+    }
       </form>
     </div>
   );
