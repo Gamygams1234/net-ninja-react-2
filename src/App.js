6e7bd3c6c797df2c31703bc334ff87030ab927bb
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-09-28 07:00:32 -0700

    Exact Router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 // routes is the version 6
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Create from "./Create";
 
 function App() {
   // adding our values at the top
@@ -15,7 +16,8 @@ function App() {
         {/* routes is the version 6 */}
         <Routes>
         
-        <Route index element={<Home />} />
+        <Route exact path='/' element={< Home />}></Route>
+        <Route exact path='/create' element={< Create />}></Route>
 
        
         </Routes>
