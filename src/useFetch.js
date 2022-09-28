92fed1cee965ce90ead2428900a221d267b71bfb
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-09-28 07:44:22 -0700

    Use effect Cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -5,8 +5,10 @@ const useFetch = (url) => {
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
 
+//   this is to make sure when we switch, it doesnt mess with the state in the home component
   useEffect(() => {
-    fetch(url)
+    const abortConst = new AbortController();
+    fetch(url, { signal: abortConst.signal })
       .then((res) => {
         if (!res.ok) {
           throw Error("could not fetch data for that resource");
@@ -20,13 +22,17 @@ const useFetch = (url) => {
         setIsPending(false);
       })
       .catch((e) => {
+        if (e.name === "AbortError") {
+          console.log("fetch aborted");
+        }
         console.log(e.message);
         setError(e.message);
         setIsPending(false);
       });
+    return () => abortConst.abort();
   }, []);
 
-  return {data, isPending, error}
+  return { data, isPending, error };
 };
 
 export default useFetch;
