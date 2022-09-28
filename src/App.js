ea6f2c5493abf2f333326086b5098e1c9cb8e704
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-09-28 06:48:06 -0700

    React router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 
 import Navbar from "./Navbar";
 import Home from "./Home";
+// routes is the version 6
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 function App() {
   // adding our values at the top
 
   return (
+    <Router>
     <div className="App">
       <Navbar></Navbar>
       <div className="content">
-      <Home></Home>
+        {/* routes is the version 6 */}
+        <Routes>
+        
+        <Route index element={<Home />} />
+
+       
+        </Routes>
+    
       </div>
    
     </div>
+    </Router>
   );
 }
 
