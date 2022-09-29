d7a4679686b3e17611617eafacfc6fe99d744e4a
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2022-09-28 22:08:25 -0700

    my solution to deleting

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,22 @@
-import { useParams } from "react-router";
+import {  useParams } from "react-router";
+import { useNavigate } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
   const { data: blog, isPending, error } = useFetch(`http://localhost:8080/blogs/${id}`);
+  const navigate = useNavigate()
+  const handleDelete = (id)=>{
+    fetch(`http://localhost:8080/blogs/${id}`, { method: "DELETE", headers: { "Content-Type": "application/json" }}).then(() => {
+        console.log(`new blog deleted`);
+   
+  
+      
+      // with react it is navigate now no longer use history
+      navigate("/")
+      });
+
+  }
   return (
     <div className="blog-details">
       {isPending && <div>Loading... </div>}
@@ -13,6 +26,7 @@ const BlogDetails = () => {
 					<h2>{blog.title}</h2>
 					<p>Written by {blog.author}</p>
 					<div>{blog.body}</div>
+                    <button onClick = {()=>{handleDelete(blog.id)}}>Delete</button>
 				</article>
 			)}
     </div>
